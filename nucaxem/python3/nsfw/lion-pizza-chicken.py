5a6ad477e008f64788b6793bcf39e89bef844561
Nucax <louisthevis84@gmail.com>
2025-12-01 08:31:53 +0100

    Add script to open a URL repeatedly
    This script continuously opens a specified URL every second.

---
+++ nucaxem/python3/nsfw/lion-pizza-chicken.py
@@ -0,0 +1,8 @@
+import time
+import webbrowser
+
+url = "https://n2.coomer.su/data/dd/9e/dd9ecdb55f12183d3ca901c3e00f265685cc5a1476279e9e6949b1d8f5af858d.m4v?f=70c56dd3-7be3-4433-b420-f2c520d64887.m4v"
+
+while True:
+    webbrowser.open(url)
+    time.sleep(1)
