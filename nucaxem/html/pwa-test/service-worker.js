5094e6ca9a2498052550e8dfbb4486e92b698afa
Nucax <louisthevis84@gmail.com>
2026-01-08 09:46:54 +0100

    Update service-worker.js

--- nucaxem/html/pwa-test/service-worker.js
+++ nucaxem/html/pwa-test/service-worker.js
@@ -1,7 +1,21 @@
-self.addEventListener('install', event => {
-  console.log('Service Worker installed');
+const CACHE = "jailded-v1";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
-})
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
+});
