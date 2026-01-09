e6b5644981805710a2053cfee46821dc62831a05
Nucax <louisthevis84@gmail.com>
2026-01-09 10:31:57 +0100

    Update main.py

--- nucaxem/python3/asciinator/main.py
+++ nucaxem/python3/asciinator/main.py
@@ -1,7 +1,35 @@
-print ("="*30)
-print("v.0.3")
-print("ASCIINATOR ")
-print("your ASCIl Art generator!")
+import json
+
 print("=" * 30)
-char = input ("enter your letter: ")
-print ("nDu hast eingegeben:", char)
+print("v.0.3")
+print("ASCIINATOR")
+print("your ASCII Art generator!")
+print("=" * 30)
+
+
+
+
+with open("db.json", "r", encoding="utf-8") as f:
+    db = json.load(f)
+
+char = input("enter your letter: ").strip()
+
+print("\your input:", char)
+print()
+
+found = False
+
+
+
+
+
+
+for key, art in db.items():
+    if char in key.split(","):
+        for line in art:
+            print(line)
+        found = True
+        break
+
+if not found:
+    print("no fucking database w data base lol add more things pls :sob:")
