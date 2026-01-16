07b22243942f2f6049ae3d72c1394720d47d7594
Nucax <louisthevis84@gmail.com>
2026-01-16 11:55:57 +0100

    Update main.py

--- nucaxem/python3/asciinator/main.py
+++ nucaxem/python3/asciinator/main.py
@@ -1,34 +1,31 @@
 import json
 
 print("=" * 30)
-print("v.0.4")
+print("v.0.5")
 print("ASCIINATOR")
 print("made by joel ganser and Jann Lübben")
 print("your ASCII Art generator!")
 print("=" * 30)
 
-
-
-
 with open("db.json", "r", encoding="utf-8") as f:
     db = json.load(f)
 
-char = input("enter your letter: ").strip()
-print("your input was :", char)
+while True:
+    char = input("\nenter your letter (or 'exit'): ").strip()
 
-found = False
-
-
-
-
-
-
-for key, art in db.items():
-    if char in key.split(","):
-        for line in art:
-            print(line)
-        found = True
+    if char.lower() == "exit":
+        print("bye")
         break
 
-if not found:
-    print("versuch etwas anderes")
+    print("your input was:", char)
+    found = False
+
+    for key, art in db.items():
+        if char in key.split(","):
+            for line in art:
+                print(line)
+            found = True
+            break
+
+    if not found:
+        print("versuch etwas anderes")
