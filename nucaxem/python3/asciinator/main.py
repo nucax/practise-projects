abadfc08917431b9df1507f9352fc17166b554f9
Nucax <louisthevis84@gmail.com>
2026-01-16 14:11:58 +0100

    Update main.py

--- nucaxem/python3/asciinator/main.py
+++ nucaxem/python3/asciinator/main.py
@@ -1,25 +1,25 @@
-# lol fix utf8
-import json
+# coded by Jann Lübben and Joel Ganser
+import json # uses json library because the ascii letters are stored in a json file
 
-print("=" * 30)
-print("v.0.5")
-print("ASCIINATOR")
-print("made by joel ganser and Jann Lübben")
+print("=" * 30) # prints 30 "="
+print("v.0.5") # version
+print("ASCIINATOR") # name of the tool
+print("made by joel ganser and Jann Lübben") # credits
 print("your ASCII Art generator!")
-print("=" * 30)
+print("=" * 30) # again prints 30 "="
 
-with open("db.json", "r", encoding="utf-8") as f:
-    db = json.load(f)
+with open("db.json", "r", encoding="utf-8") as f: # conf the load
+    db = json.load(f) # load the .json
 
-while True:
-    char = input("\nenter your letter (or exit): ").strip()
+while True: #infinite loop
+    # working because it does not need anything after loading the db
+    char = input("\nenter your letter (or exit): ").strip() # asks for input (letter and also exit)
 
-    if char.lower() == "exit":
-        print("bye")
-        break
+    if char.lower() == "exit": # exit
+        break # closes
 
     print("your input was:", char)
-    found = False
+    found = False # get found false
 
     for key, art in db.items():
         if char in key.split(","):
@@ -29,4 +29,4 @@ while True:
             break
 
     if not found:
-        print("versuch etwas anderes")
+        print("versuch etwas anderes") # get the user to try something different
